e1a9cb1756d60e57d7fc17f001832db69ef977e0
cmackie21 <catherine.mackie@hotmail.co.uk>
2016-10-26 16:18:54 +0100

    Added jquery events for buttons in Projects section

---
+++ cm-jq.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $("#resh-button").click( function(event){
+		$("#esh-panel").fadeToggle();
+	});
+	
+	$("#java-button").click( function(event){
+		$("#java-panel").fadeToggle();
+	});
+	
+	$("#android-button").click( function(event){
+		$("#android-panel").fadeToggle();
+	});
+	
+	$("#book-button").click( function(event){
+		$("#book-panel").fadeToggle();
+	});
+	
+
+});
